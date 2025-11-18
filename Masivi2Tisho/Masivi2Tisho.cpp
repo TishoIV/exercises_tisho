862cecee6c313672fcf3e1dda8d2905192ddf9cd
TishoIV <154275664+TishoIV@users.noreply.github.com>
2025-11-18 13:22:05 +0200

    Add files via upload

---
+++ Masivi2Tisho/Masivi2Tisho.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	int arr[10];
+	int value;
+	
+	
+}
+
+
---
+++ MasiviTisho/MasiviTisho.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int value; // дефинираме стойност value в целочислен тип
+	int arr[10]; // дефинираме едномерен масив с 10 елемента
+	for (int i = 0; i < 10; i++) 
+	{
+		scanf_s("%d", &value); 
+		arr[i] = value; 
+	}
+	printf("\n");
+	for (int i = 0; i < 10; i++) 
+	{
+		printf("arr[%d]=%d\n", i, arr[i]); // принтираме i arr[i]
+	}
+	for (int i = 10 - 1; i > 0; i--)
+	{
+		printf("arr[%d]=%d\n", i, arr[i]);
+	}
+}
+
+
